e617f18c31286248c27e5f64fa0eb0d80df56dd4
Guilherme Decampo <guilherme.decampo@gmail.com>
2016-05-17 15:03:21 -0300

    Add new logic using directly the correios API

--- nice-correios.js
+++ nice-correios.js
@@ -3,11 +3,56 @@ NiceCorreios.getDeliveryCost = function(options){
   check(options, {
     from: String,
     to: String,
-    weight: Match.OneOf(String, Number)
+    weight: Match.OneOf(String, Number),
   });
 
-  var call = 'http://developers.agenciaideias.com.br/correios/frete/json/';
-  call += options.from + '/' + options.to + '/' + options.weight.toString();
+  const soap = Npm.require('soap');
+  const Future = Npm.require('fibers/future');
+  const future = new Future();
+  const apiUrl = 'http://ws.correios.com.br/calculador/CalcPrecoPrazo.asmx?wsdl';
 
-  return EJSON.parse(HTTP.get(call).content);
+  const {from, to, weight} = options;
+  const args = {
+    nCdEmpresa: '',
+    sDsSenha: '',
+    sCdMaoPropria: 'N',
+    nVlValorDeclarado : 0,
+    sCdAvisoRecebimento: 'N',
+    nCdServico: '40010, 41106',
+
+    // User info,
+    sCepOrigem: from,
+    sCepDestino: to,
+    nVlPeso: weight,
+
+    // Standard package format
+    nCdFormato: 1,
+
+    // Standard package size
+    nVlComprimento: 27,
+    nVlAltura: 22,
+    nVlLargura: 13,
+    nVlDiametro: 0,
+  };
+
+
+  soap.createClient(apiUrl, (error, client) => {
+    client.CalcPreco(args, (error, result) => {
+      if (error) {
+        future.throw(error);
+        return;
+      }
+
+      const services = result.CalcPrecoResult.Servicos.cServico;
+      const sedex = (_.find(services, {Codigo: 40010}) || {}).Valor || 0;
+      const pac = (_.find(services, {Codigo: 41106}) || {}).Valor || 0;
+
+      future.return({
+        sedex,
+        pac,
+      });
+    });
+  });
+
+  return future.wait();
 };
