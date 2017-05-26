a9ccd45b2663f85333c0e39ac5724bd6884bca19
Guilherme Decampo <guilherme.decampo@gmail.com>
2017-05-26 12:27:50 -0300

    Bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'astrocoders:nice-correios',
-  version: '0.1.1',
+  version: '0.1.2',
   summary: 'Get Correios shipping costs without complications',
   git: 'https://github.com/Astrocoders/meteor-nice-correios',
   documentation: 'README.md'
@@ -14,18 +14,12 @@ Package.onUse(function(api) {
   api.versionsFrom('1.1.0.3');
 
   api.use([
-    'ecmascript@0.1.5',
+    'ecmascript',
     'check',
-    'stevezhu:lodash@3.10.1',
+    'stevezhu:lodash',
   ], 'server');
 
   api.addFiles('nice-correios.js', 'server');
 
   api.export('NiceCorreios');
-});
-
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('astrocoders:nice-correios');
-  api.addFiles('nice-correios-tests.js');
 });
